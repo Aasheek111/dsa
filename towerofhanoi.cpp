37ae023cd93583a2d55d1a35cf3fcb39e9e01583
Aashik Gautam <technicalaashik111@gmail.com>
2025-12-20 16:56:50 +0545

    tower of hanoi

---
+++ towerofhanoi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void tower(int index, char source, char destination, char aux){
+
+    if(index==1){
+        cout<<"Move 1 from "<<source <<" TO " <<destination<<endl;
+        return;
+    }
+
+    tower(index-1,source,aux,destination);
+        cout<<"Move "<<index<<" From "<<source<<" TO "<<destination<<endl;
+        tower(index-1,aux,destination,source);
+
+}
+
+int main(){
+
+    tower(3,'A','C','B');
+    return 0;
+
+}
