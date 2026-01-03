e5ff5e5b593538600b42ea091c693c11b09fbbd8
Aashik Gautam <technicalaashik111@gmail.com>
2026-01-03 13:22:31 +0545

    linkedlist

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,16 +1,76 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data ;
-    Node* next;
-}
-Node* head;
-void insertFirst(int data){
-    
+// we will do insert in front, last and at middle and delete
+struct Node
+{
+    int data;
+    Node *next;
+};
 
-}
-void main(){
-    
+class Linkedlist
+{
 
+    Node *head;
+
+public:
+    Linkedlist()
+    {
+        head = NULL;
+    }
+
+    void insertFront(int data)
+    {
+        Node *newnode = new Node();
+        newnode->data = data;
+        newnode->next = head;
+        head = newnode;
+    }
+    void insertBack(int data)
+    {
+
+        Node *newnode = new Node();
+        newnode->data = data;
+        newnode->next = NULL;
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+    }
+
+    // this inserts the data aftet the pos
+    // pos means index
+    void insertMid(int data, int pos)
+    {
+        Node *newnode = new Node();
+        newnode->data = data;
+        Node *temp = head;
+
+        for (int i = 0; i < pos && temp != NULL; i++)
+        {
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+    void display()
+    {
+        Node *tempnode = head;
+        while (tempnode->next != NULL)
+        {
+            cout << tempnode->data << endl;
+            tempnode = tempnode->next;
+        }
+    }
+};
+int main()
+{
+
+    Linkedlist l1;
+    l1.insertFront(12);
+    l1.insertFront(14);
+    l1.insertFront(15);
+    l1.display();
 }
